94b38650c39e931261e4f0c046ceeceed9acf14d
xiaziheng <1192939559@qq.com>
2022-09-20 10:08:46 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## Personal for study
---
+++ xzh/GetDate.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace xzh;
+
+class GetDate
+{
+    public function getNowDate()
+    {
+        return date('Y-m-d H:i:s');
+    }
+
+    public function test()
+    {
+        return 'qwerqwrasasf';
+    }
+}
+
+
